4a41e3436d819e461f05f0fc8aa0809b961360e6
pradeep kumar <pradesep6@gmail.com>
2016-11-29 00:57:36 +0530

    Initialized

---
+++ Standup/Standup/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Standup
+//
+//  Created by pradeep on 28/11/16.
+//  Copyright © 2016 pradeep. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
