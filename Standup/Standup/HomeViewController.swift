cb22614469022b484281be3862a31984d839bacc
prade6 <pradesep6@gmail.com>
2016-11-30 20:01:30 +0530

    modified

--- Standup/Standup/HomeViewController.swift
+++ Standup/Standup/HomeViewController.swift
@@ -19,12 +19,34 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let v1 = PendingViewController(nibName: "PendingViewController", bundle: nil)
+        let v2 = CurrentViewController(nibName: "CurrentViewController", bundle: nil)
+        let v3 = DoneViewController(nibName: "DoneViewController", bundle: nil)
+        
         
         self.addChildViewController(v1)
         self.gestureScrollView.addSubview(v1.view)
         v1.didMove(toParentViewController: self)
         
-    
+        self.addChildViewController(v2)
+        self.gestureScrollView.addSubview(v2.view)
+        v2.didMove(toParentViewController: self)
+        
+        self.addChildViewController(v3)
+        self.gestureScrollView.addSubview(v3.view)
+        v3.didMove(toParentViewController: self)
+        
+        
+        var v2Frame : CGRect = v2.view.frame
+        v2Frame.origin.x = self.view.frame.width
+        v2.view.frame = v2Frame
+        
+        var v3Frame : CGRect = v3.view.frame
+        v3Frame.origin.x = 2 * self.view.frame.width
+        v3.view.frame = v3Frame
+        
+        
+        self.gestureScrollView.contentSize = CGSize(width: self.view.frame.width * 3, height: gestureScrollView.frame.height)
+        
     }
 
     override func didReceiveMemoryWarning() {
